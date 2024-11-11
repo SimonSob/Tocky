ebdf6e084a019bfcf72f0c5ec13cfadaf0a49e93
Sobotka Šimon <L20220051@SSPTAJI.CZ>
2024-11-11 12:48:31 +0100

    nwcoo

---
+++ app.js
@@ -0,0 +1,49 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(error => {
+                console.log('Service Worker registration failed:', error);
+            });
+    });
+}
+
+function getRandomZnak(zasobnik_znaku)
+{
+    const num = Math.random() * (zasobnik_znaku.lenght-1);
+    return(zasobnik_znaku[num]);
+}
+
+function Roztoc()
+{
+    let zasobnik_znaku=["⭐","🍀","🌈","🌸"]
+    const refresh_rate1 = 100;
+    const refresh_rate2 = 200;
+    const refresh_rate3 = 300;
+
+    for(let i=0;i<20000;i++)
+    {
+        if(i % refresh_rate1 == 0)
+        {
+            console.log(getRandomZnak(zasobnik_znaku));
+            document.getElementById("first").textContent = getRandomZnak(zasobnik_znaku);
+        }
+
+        if(i % refresh_rate2 == 0)
+        {
+            
+            document.getElementById("second").textContent = getRandomZnak(zasobnik_znaku);
+        }
+
+        if(i % refresh_rate3 == 0)
+        {
+            
+            document.getElementById("third").textContent = getRandomZnak(zasobnik_znaku);
+        }
+    }
+
+    document.getElementById("first").textContent;  
+
+}
